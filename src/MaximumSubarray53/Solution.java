b6230f8e72e6fb0a0516cbee3cb319fd3f028b4d
Asmar06 <asmar.ismayilova.06@outlook.com>
2021-10-12 10:34:32 +0400

    commit

---
+++ src/MaximumSubarray53/Solution.java
@@ -0,0 +1,15 @@
+package MaximumSubarray53;
+
+public class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxSum = nums[0];
+        int currSum = maxSum;
+
+        for (int i = 1; i < nums.length; i++) {
+            currSum = Math.max(nums[i] + currSum, nums[i]);
+            maxSum = Math.max(currSum, maxSum);
+        }
+
+        return maxSum;
+    }
+}
